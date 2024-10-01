967b5f97198ed562174db1437d140f54ac9e4ef8
Ryan <75371366+jyun19980812@users.noreply.github.com>
2024-10-01 11:28:49 +0900

    [level 4] Title: 언어별 개발자 분류하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/4/276036. 언어별 개발자 분류하기/언어별 개발자 분류하기.sql
+++ 프로그래머스/4/276036. 언어별 개발자 분류하기/언어별 개발자 분류하기.sql
@@ -1,21 +1,44 @@
-WITH CTE AS (
-    SELECT SUM(CODE)
-    FROM SKILLCODES
-    WHERE CATEGORY = 'Front End'
+/*
+    SKILLCODES: 프로그래밍 언어 정보 테이블, DEVELOPERS= 개발자들의 프로그래밍 스킬 정보
+    A : Front End 스킬과 Python 스킬을 함께 가지고 있는 개발자
+    B : C# 스킬을 가진 개발자
+    C : 그 외의 Front End 개발자
+    GRADE가 존재하는 개발자의 GRADE, ID, EMAIL 조회하는 SQL문 작성
+    출력: GRADE, ID, EMAIL, ORDER BY GRADE, ID
+    CTE로 각 조건에 맞게 개발자 분류, 특히 A의 경우 파이썬과 Front End가 둘다 있을 경우여야 
+    하기 때문에 Join을 활용, 마지막 CTE에서 GRADE ID별로 계산,
+    밖에 SELECT 문에서 GRADE가 NULL이 아닌것만 가져오고 정렬
+*/
+WITH A AS (
+    SELECT d.ID
+    FROM DEVELOPERS d
+        JOIN SKILLCODES s1 ON s1.code & d.skill_code != 0 AND s1.category = "Front End"
+        JOIN SKILLCODES s2 ON s2.code & d.skill_code != 0 AND s2.name = "Python"
+),
+B AS (
+    SELECT DISTINCT ID
+    FROM DEVELOPERS, SKILLCODES
+    WHERE NAME = 'C#' AND 
+          CODE & SKILL_CODE > 0
+),
+C AS (
+    SELECT DISTINCT ID
+    FROM DEVELOPERS, SKILLCODES
+    WHERE CATEGORY = "Front End" AND 
+          CODE & SKILL_CODE > 0
+),
+CTE AS (
+    SELECT 
+        CASE
+            WHEN ID IN (SELECT ID FROM A) THEN "A"
+            WHEN ID IN (SELECT ID FROM B) THEN "B"
+            WHEN ID IN (SELECT ID FROM C) THEN "C"
+        END AS GRADE,
+        ID,
+        EMAIL
+    FROM DEVELOPERS
 )
-
-SELECT 
-    CASE
-        WHEN SKILL_CODE & (SELECT * FROM CTE)
-        AND SKILL_CODE & (SELECT CODE FROM SKILLCODES WHERE NAME = 'python')
-        THEN 'A'
-    WHEN SKILL_CODE & (SELECT CODE FROM SKILLCODES WHERE NAME = 'C#') 
-        THEN 'B'
-    WHEN SKILL_CODE & (SELECT * FROM CTE)
-        THEN 'C'
-    END AS GRADE, ID, EMAIL
-FROM DEVELOPERS D
-    JOIN SKILLCODES S ON D.SKILL_CODE & S.CODE = S.CODE
-GROUP BY GRADE, ID, EMAIL
-HAVING GRADE IS NOT NULL
+SELECT *
+FROM CTE
+WHERE GRADE IS NOT NULL
 ORDER BY GRADE, ID
