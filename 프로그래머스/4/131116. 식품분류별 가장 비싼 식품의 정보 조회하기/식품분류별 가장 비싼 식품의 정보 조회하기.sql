da14525c7ee5526ee4809e2fdae9a6ff73db7aa0
Ryan <75371366+jyun19980812@users.noreply.github.com>
2024-10-02 10:49:56 +0900

    [level 4] Title: 식품분류별 가장 비싼 식품의 정보 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/131116. 식품분류별 가장 비싼 식품의 정보 조회하기/식품분류별 가장 비싼 식품의 정보 조회하기.sql
+++ 프로그래머스/4/131116. 식품분류별 가장 비싼 식품의 정보 조회하기/식품분류별 가장 비싼 식품의 정보 조회하기.sql
@@ -1,12 +1,27 @@
--- 코드를 입력하세요
+/*
+    문제: 식품분류별로 가격이 제일 비싼 식품의 분류, 가격, 이름을 조회하는 SQL문을 작성
+    조건: 식품분류가  '과자', '국', '김치', '식용유'인 경우만 출력, ORDER BY 식품 가격
+    출력: CATEGORY, MAX_PRICE, PRODUCT_NAME
+    CTE문에서 GROUP BY CATEGORY, MAX(PRICE)를 찾아 놓고,
+    밖에 SELECT문에서 필요한 정보 출력하는데, WHERE문에서 조건에 맞춰서 출력
+*/
 WITH CTE AS (
-    SELECT CATEGORY, MAX(PRICE) AS MAX_PRICE 
-    FROM FOOD_PRODUCT
-    GROUP BY CATEGORY
+    SELECT
+        PRODUCT_ID,
+        MAX(PRICE) AS N_PRICE
+    FROM 
+        FOOD_PRODUCT
+    GROUP BY 
+        CATEGORY
 )
-SELECT FP.CATEGORY, C.MAX_PRICE, FP.PRODUCT_NAME
-FROM FOOD_PRODUCT FP
-    JOIN CTE C ON FP.CATEGORY = C.CATEGORY
-               AND FP.PRICE = C.MAX_PRICE
-WHERE FP.CATEGORY IN ("과자", '국', '김치', '식용유')
-ORDER BY FP.PRICE DESC
+SELECT
+    CATEGORY,
+    PRICE AS MAX_PRICE,
+    PRODUCT_NAME
+FROM
+    FOOD_PRODUCT
+WHERE 
+    PRICE IN (SELECT N_PRICE FROM CTE) AND
+    CATEGORY IN ('과자', '국', '김치', '식용유')
+ORDER BY 
+    MAX_PRICE DESC
