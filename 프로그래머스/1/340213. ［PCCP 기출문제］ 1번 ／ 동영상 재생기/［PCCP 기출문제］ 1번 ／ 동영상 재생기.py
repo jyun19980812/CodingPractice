f2db667f9039f8a38a10e78f8a1a46a3e0ce9e9d
Ryan <75371366+jyun19980812@users.noreply.github.com>
2024-10-08 14:58:46 +0900

    [level 1] Title: [PCCP 기출문제] 1번 / 동영상 재생기, Time: 0.05 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/1/340213. ［PCCP 기출문제］ 1번 ／ 동영상 재생기/［PCCP 기출문제］ 1번 ／ 동영상 재생기.py
+++ 프로그래머스/1/340213. ［PCCP 기출문제］ 1번 ／ 동영상 재생기/［PCCP 기출문제］ 1번 ／ 동영상 재생기.py
@@ -1,30 +1,17 @@
 def solution(video_len, pos, op_start, op_end, commands):
-    answer = ''
-    video_len = int(video_len[0:2]) * 60 + int(video_len[3:])
-    pos = int(pos[0:2]) * 60 + int(pos[3:])
-    op_start = int(op_start[0:2]) * 60 + int(op_start[3:])
-    op_end = int(op_end[0:2]) * 60 + int(op_end[3:])
-    if pos >= op_start and pos <= op_end:
-        pos = op_end 
-    for command in commands:   
-        if command == "prev":
-            pos -= 10
-            if pos < 0:
-                pos = 0
-        else:
-            pos += 10
-            if pos > video_len:
-                pos = video_len
-        if pos >= op_start and pos <= op_end:
-            pos = op_end
-    h = pos // 60
-    m = pos % 60
-    if h < 10:
-        answer = "0" + str(h) + ":"
-    else:
-        answer = str(h) + ":"
-    if m < 10:
-        answer += "0" + str(m)
-    else:
-        answer += str(m)
-    return answer
+    v = 60 * int(video_len.split(":")[0]) + int(video_len.split(":")[1])
+    p = 60 * int(pos.split(":")[0]) + int(pos.split(":")[1]) 
+    s = 60 * int(op_start.split(":")[0]) + int(op_start.split(":")[1])
+    e = 60 * int(op_end.split(":")[0]) + int(op_end.split(":")[1])
+    for command in commands:
+        if s <= p <= e:
+            p = e
+        if command == 'next':
+            p = min(p + 10, v)
+        elif command == 'prev':
+            p = max(p - 10, 0)
+    if s <= p <= e:
+        p = e
+    m = str(p // 60) if p // 60 >= 10 else f"0{p//60}"
+    s = str(p % 60) if p % 60 >= 10 else f"0{p%60}"
+    return m + ":" + s
