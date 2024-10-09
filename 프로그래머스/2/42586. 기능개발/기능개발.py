e40680bd2658c52473b48c907d3e7f97adbe2b4e
Ryan <75371366+jyun19980812@users.noreply.github.com>
2024-10-09 14:21:45 +0900

    [level 2] Title: 기능개발, Time: 0.07 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.py
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -1,20 +1,17 @@
-import math
-
 def solution(progresses, speeds):
-    p = progresses[0]
-    s = speeds[0]
-    publish = math.ceil((100-p) / s)
-    count = 1
     answer = []
-    for i in range(1, len(progresses)):
-        p = progresses[i]
-        s = speeds[i]
-        next_p = math.ceil((100-p) / s)
-        if publish >= next_p:
-            count += 1;
+    time = 0
+    count = 0
+    while len(progresses) > 0:
+        if progresses[0] + speeds[0] * time < 100:
+            if count > 0:
+                answer.append(count)
+                count = 0
+            time += 1
         else:
-            publish = next_p
-            answer.append(count)
-            count = 1
+            progresses.pop(0)
+            speeds.pop(0)
+            count += 1
     answer.append(count)
     return answer
+
