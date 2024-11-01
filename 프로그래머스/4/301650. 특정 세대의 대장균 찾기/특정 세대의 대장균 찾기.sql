b113507530041a564cda2a7fb7875f4c45fd57cf
Ryan <75371366+jyun19980812@users.noreply.github.com>
2024-11-01 22:29:07 +0900

    [level 4] Title: 특정 세대의 대장균 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/4/301650. 특정 세대의 대장균 찾기/특정 세대의 대장균 찾기.sql
+++ 프로그래머스/4/301650. 특정 세대의 대장균 찾기/특정 세대의 대장균 찾기.sql
@@ -1,14 +1,23 @@
-WITH 2nd_Gen AS (
-    SELECT B.ID, B.PARENT_ID
-    FROM ECOLI_DATA A
-        JOIN ECOLI_DATA B ON A.ID = B.PARENT_ID
-),
-3rd_Gen AS (
-    SELECT B.ID, B.PARENT_ID
-    FROM 2nd_Gen A
-        JOIN 2nd_Gen B ON A.ID = B.PARENT_ID
+/*
+    3세대 대장균의 id를 출력하는 문제 (1세대 -> parent id가 NULL, 2세대 -> parent_id가 1세대, 3세대 -> 2세대)
+    출력: ID, ORDER BY ID
+    CTE로 SELF JOIN을 해서 1세대에 해당하는 ID를 파악, 그러고 같은 방식으로 3세대 파악
+    이후 밖에 SELECT문에서 출력
+*/
+WITH CTE AS (
+    SELECT 
+        A.ID
+    FROM
+        ECOLI_DATA A
+        JOIN ECOLI_DATA B ON A.PARENT_ID = B.ID
+    WHERE
+        B.PARENT_ID IS NULL
 )
-SELECT ID
-FROM 3rd_Gen
-WHERE PARENT_ID NOT IN (SELECT ID 
-                        FROM 3rd_Gen) 
+SELECT
+    ID
+FROM
+    ECOLI_DATA
+WHERE
+    PARENT_ID IN (SELECT ID FROM CTE)
+ORDER BY
+    ID ASC
