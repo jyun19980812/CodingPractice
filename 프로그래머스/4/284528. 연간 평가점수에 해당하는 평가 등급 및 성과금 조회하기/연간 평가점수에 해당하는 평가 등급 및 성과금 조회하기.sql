808865c3237a79d8b8f4ef9b09934beda9f970b0
Ryan <75371366+jyun19980812@users.noreply.github.com>
2024-11-16 13:13:13 +0900

    [level 4] Title: 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/4/284528. 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기/연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기.sql
+++ 프로그래머스/4/284528. 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기/연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기.sql
@@ -1,21 +1,37 @@
+/*
+    HR_DEPARTMENT: 회사 부서 정보 테이블, HR_EMPLOYEES: 회사 사원 정보 테이블, 
+    HR_GRADE: 2022년 사원 평가 정보 테이블
+    목표: 평가 점수별 등급과 등급에 따른 사원별 성과금 정보 조회
+    96 이상, S등급, 20% / 90 이상, A, 15% / 80 이상, B, 10% / 이외, C, 0%
+    출력: EMP_NO, EMP_NAME, GRADE, BONUS
+    CTE로 HR_GRADE에서 GROUP BY EMP_NO AVG(SCORE)을 해주고, AVG(SCORE) 별로 등급 매겨주고
+    밖에 SELECT문에서 HR_EMPLOYEE와 조인 후 점수에 따라 보너스 계산
+*/
 WITH CTE AS (
-    SELECT EMP_NO, AVG(SCORE) AS SCORE
-    FROM HR_GRADE
-    GROUP BY EMP_NO
+    SELECT
+        EMP_NO,
+        AVG(SCORE) AS SCORE,
+        CASE
+            WHEN AVG(SCORE) >= 96 THEN "S"
+            WHEN AVG(SCORE) >= 90 THEN "A"
+            WHEN AVG(SCORE) >= 80 THEN "B"
+            ELSE "C"
+        END AS GRADE
+    FROM
+        HR_GRADE
+    GROUP BY
+        EMP_NO
 )
-SELECT HE.EMP_NO, EMP_NAME, 
+SELECT
+    HE.EMP_NO,
+    HE.EMP_NAME,
+    C.GRADE,
     CASE
-        WHEN SCORE >= 96 THEN "S"
-        WHEN SCORE < 96 AND SCORE >= 90 THEN "A"
-        WHEN SCORE < 90 AND SCORE >= 80 THEN "B"
-        ELSE "C"
-    END AS GRADE,
-    CASE
-        WHEN SCORE >= 96 THEN SAL * 0.2
-        WHEN SCORE < 96 AND SCORE >= 90 THEN SAL * 0.15
-        WHEN SCORE < 90 AND SCORE >= 80 THEN SAL * 0.1
-        ELSE 0
+        WHEN C.GRADE = "S" THEN SAL * 0.2
+        WHEN C.GRADE = "A" THEN SAL * 0.15
+        WHEN C.GRADE = "B" THEN SAL * 0.1
+        ELSE SAL * 0
     END AS BONUS
-FROM HR_EMPLOYEES HE
+FROM 
+    HR_EMPLOYEES HE
     JOIN CTE C ON HE.EMP_NO = C.EMP_NO
-ORDER BY HE.EMP_NO ASC
