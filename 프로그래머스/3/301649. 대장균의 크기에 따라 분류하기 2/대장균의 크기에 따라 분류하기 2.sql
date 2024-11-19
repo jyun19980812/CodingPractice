e2b48797f764eef134cced0e6d8df6380dfc024a
Ryan <75371366+jyun19980812@users.noreply.github.com>
2024-11-19 21:01:33 +0900

    [level 3] Title: 대장균의 크기에 따라 분류하기 2, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/3/301649. 대장균의 크기에 따라 분류하기 2/대장균의 크기에 따라 분류하기 2.sql
+++ 프로그래머스/3/301649. 대장균의 크기에 따라 분류하기 2/대장균의 크기에 따라 분류하기 2.sql
@@ -1,12 +1,24 @@
+/*
+    대장균 개체 ID와 분류된 이름 (크기 카테고리) 출력, ORDER BY ID ASC
+    25% = CRITICAL, 26~50% = HIGH, 51~75% MEDIUM, 76~100% LOW
+    CTE에서 PERCENT_RANK()로 상위 % 구하기, 이후 SELECT문에서 CASE WHEN으로 크기 카테고리 계산
+*/
 WITH CTE AS (
-    SELECT ID, PERCENT_RANK() OVER (ORDER BY SIZE_OF_COLONY DESC) AS PERCENT
-    FROM ECOLI_DATA)
-SELECT ID,
-        CASE
-            WHEN PERCENT <= 0.25 THEN 'CRITICAL'
-            WHEN PERCENT > 0.25 AND PERCENT <= 0.5 THEN 'HIGH'
-            WHEN PERCENT > 0.5 AND PERCENT <= 0.75 THEN 'MEDIUM'
-            ELSE 'LOW'
-        END AS COLONY_NAME
-FROM CTE
-ORDER BY ID
+    SELECT
+        ID,
+        PERCENT_RANK() OVER (ORDER BY SIZE_OF_COLONY DESC) AS rk
+    FROM
+        ECOLI_DATA
+)
+SELECT
+    ID,
+    CASE
+        WHEN rk >= 0.0 AND rk <= 0.25 THEN 'CRITICAL'
+        WHEN rk > 0.25 AND rk <= 0.5 THEN 'HIGH'
+        WHEN rk > 0.5 AND rk <= 0.75 THEN 'MEDIUM'
+        ELSE 'LOW'
+    END AS COLONY_NAME
+FROM
+    CTE
+ORDER BY
+    ID
