e36f14e2ae37b445b8630e452b5e168cd1e3fb7a
Ryan <75371366+jyun19980812@users.noreply.github.com>
2024-12-04 02:37:37 +0900

    [level 4] Title: 보호소에서 중성화한 동물, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/59045. 보호소에서 중성화한 동물/보호소에서 중성화한 동물.sql
+++ 프로그래머스/4/59045. 보호소에서 중성화한 동물/보호소에서 중성화한 동물.sql
@@ -1,9 +1,19 @@
-# ANIMAL_INS는 동물 보호소에 들어온 동물의 정보 테이블, ANIMAL_OUTS는 동물 보호소에서 입양 보낸 동물의 정보 테이블
-# 보호소에서 중성화 수술을 거친 동물 정보 조회
-# ANIMAL_ID, ANIMAL_TYPE, NAME 조회, NAME순으로 ORDER BY
-# 두 테이블을 아이디를 기준으로 조인, SEX_UPON_INTAKE와 SEX_UPON_OUTCOME이 다를경우 프린트
-SELECT AI.ANIMAL_ID, AI.ANIMAL_TYPE, AI.NAME
-FROM ANIMAL_INS AI
+/*
+    목표: 보호소에 들어올 당시엔 중성화 X, 보호소 나갈땐 중성화된 동물의 정보 조회
+    출력: ANIMAL_ID, ANIMAL_TYPE, NAME, ORDER BY ANIMAL_ID
+    먼저 ANIMAL_INS와 ANIMAL_OUTS 조인, 이후 중성화 여부를 이전 테이블에는 Intact가,
+    이후 테이블에는 Spayed나 Neutered가 포함되어있는 지 파악해서 풀기
+*/
+SELECT
+    AI.ANIMAL_ID,
+    AI.ANIMAL_TYPE,
+    AI.NAME
+FROM
+    ANIMAL_INS AI
     JOIN ANIMAL_OUTS AO ON AI.ANIMAL_ID = AO.ANIMAL_ID
-WHERE SEX_UPON_INTAKE <> SEX_UPON_OUTCOME
-ORDER BY AI.ANIMAL_ID
+WHERE
+    AI.SEX_UPON_INTAKE LIKE "%Intact%" AND
+    (AO.SEX_UPON_OUTCOME LIKE "%Spayed%" OR 
+     AO.SEX_UPON_OUTCOME LIKE "%Neutered%")
+ORDER BY
+    AI.ANIMAL_ID
