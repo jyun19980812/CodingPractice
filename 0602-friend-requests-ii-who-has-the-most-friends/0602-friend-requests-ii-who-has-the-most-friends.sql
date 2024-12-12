e09cdc54897f73095813f8a4fff51dbec3d383b4
Ryan <75371366+jyun19980812@users.noreply.github.com>
2024-12-12 16:01:34 +0900

    Time: 547 ms (6.49%), Space: 0B (100.00%) - LeetHub

--- 0602-friend-requests-ii-who-has-the-most-friends/0602-friend-requests-ii-who-has-the-most-friends.sql
+++ 0602-friend-requests-ii-who-has-the-most-friends/0602-friend-requests-ii-who-has-the-most-friends.sql
@@ -1,9 +1,3 @@
-/*
-    목표: 테이블에 있는 사람 중 가장 많은 친구를 가진 사람 출력
-    requester_id나 accpeter_id에서 가장 많이 노출되는 id가 친구가 많은 id
-    CTE로 accepter_id와 requester_id 하나의 컬럼으로, union으로 결합
-    이후 밖에 SELECT문에서 id로 GROUPBY 하고, 카운트
-*/
 WITH CTE AS (
     SELECT 
         requester_id AS id
