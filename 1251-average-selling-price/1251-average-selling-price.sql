8635f2aefe6ba15f5f9c85bac7c67015f40ce0ad
Ryan <75371366+jyun19980812@users.noreply.github.com>
2024-12-20 23:40:04 +0900

    Time: 1149 ms (15.76%), Space: 0B (100.00%) - LeetHub

--- 1251-average-selling-price/1251-average-selling-price.sql
+++ 1251-average-selling-price/1251-average-selling-price.sql
@@ -1,23 +1,19 @@
 /*
     목표:각 제품별로 평균값을 찾아라
     출력: product_id, average_price
-    2개의 테이블을 product_id와 purchase_date이 start_date과 end_date 중간에 있는지를 기준으로 조인
+    2개의 테이블을 product_id와 purchase_date이 start_date과 end_date 중간에 있는지를 기준으로 
+    LEFT 조인 (units가 없을 수도 있기 때문)
+    이후 product_id를 그룹해주고 CASE WHEN으로 유닛이 없으면 0, 아니면 average 계산 
 */
-SELECT
-    product_id,
+SELECT 
+    P.product_id,
     CASE
         WHEN SUM(units) IS NULL THEN 0
-        ELSE ROUND(SUM(profit) / SUM(units), 2) 
+        ELSE ROUND(SUM(price * units) / SUM(units), 2) 
     END AS average_price
-FROM (
-    SELECT
-        P.product_id,
-        US.units,
-        price * units AS profit
-    FROM
-        Prices P
-        LEFT JOIN UnitsSold US ON P.product_id = US.product_id
-                         AND US.purchase_date BETWEEN P.start_date AND P.end_date
-    ) tb
+FROM 
+    Prices P
+    LEFT JOIN UnitsSold US ON P.product_id = US.product_id
+                           AND purchase_date BETWEEN start_date AND end_date
 GROUP BY
-    P.product_id
+    product_id
