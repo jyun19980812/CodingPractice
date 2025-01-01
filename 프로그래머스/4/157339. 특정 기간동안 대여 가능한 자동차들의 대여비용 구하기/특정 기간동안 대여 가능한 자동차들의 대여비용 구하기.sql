2be089a284b77258383f10efc58c9d0c3c73a6a1
Ryan <75371366+jyun19980812@users.noreply.github.com>
2025-01-01 17:53:25 +0900

    [level 4] Title: 특정 기간동안 대여 가능한 자동차들의 대여비용 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/157339. 특정 기간동안 대여 가능한 자동차들의 대여비용 구하기/특정 기간동안 대여 가능한 자동차들의 대여비용 구하기.sql
+++ 프로그래머스/4/157339. 특정 기간동안 대여 가능한 자동차들의 대여비용 구하기/특정 기간동안 대여 가능한 자동차들의 대여비용 구하기.sql
@@ -1,48 +1,29 @@
 /*
-    CAR_RENTAL_COMPANY_CAR: 대여 중인 자동차들의 정보, 
-    CAR_RENTAL_COMPANY_RENTAL_HISTORY: 자동차 대여 기록 정보,
-    CAR_RENTAL_COMPANY_DISCOUNT_PLAN: 자동차 종류 별 대여 기간 종류 별 할인 정책 정보
     자동차 종류가 '세단' 또는 'SUV'인 자동차 중에, 2022/11/01 ~ 2022/11/30까지 대여가 가능하고
     30일간의 대여금액이 50만원 이상 200만원 미만인 자동차에 대해서 정보 출력
     출력: CAR_ID, CAR_TYPE, FEE, ORDER BY FEE DESC, CAR_TYPE ASC, CAR_ID DESC
-    CTE에서, CAR_RENTAL_COMPANY_CAR이랑, CAR_RENTAL_COMPANY_RENTAL_HISTORY 조인, 기준은 CAR_ID
-    WHERE문에서 CAR_TYPE이 SUV나 세단인걸로 필터하고, CAR_RENTAL_HISTORY는 CAR_ID 그룹해서 MAX(END_DATE)하고
-    이게 2022-11-01~2022-11-30일안이 아닌걸로 필터링
-    밖에서 또 조인, 조인 기준은 CAR_TYPE인데 DURATION_TYPE이 30일 이상으로 적혀있는 걸로
-    FEE를 계산
+   먼저 CTE로 CAR_RENTAL_COMPANY_RENTAL_HISTORY에서 대여 불가한 아이디 넘버를 구해놓고,
+   CAR_RENTAL_COMPANY_CAR에서 대여 불가한 아이디가 없는것으로 필터링 한다음,
+   CAR_RENTAL_COMPANY_DISCOUNT_PLAN과 조인 후 대여금액 계산
 */
 WITH CTE AS (
-    SELECT 
-        CRCC.CAR_ID, 
-        CRCC.CAR_TYPE, 
-        CRCC.DAILY_FEE
-    FROM 
-        CAR_RENTAL_COMPANY_CAR CRCC
-        LEFT JOIN CAR_RENTAL_COMPANY_RENTAL_HISTORY CRCRH ON CRCC.CAR_ID = CRCRH.CAR_ID
-                                                     AND START_DATE <= "2022-11-30" 
-                                                     AND END_DATE >= "2022-11-01"
-    WHERE CRCC.CAR_TYPE IN ('SUV', '세단') AND HISTORY_ID IS NULL
+    SELECT DISTINCT CAR_ID
+    FROM CAR_RENTAL_COMPANY_RENTAL_HISTORY
+    WHERE START_DATE <= "2022-11-30" AND END_DATE >= "2022-11-01"
 ),
 CTE2 AS (
-    SELECT 
-        C.CAR_ID,
-        C.CAR_TYPE, 
-        CRCDP.DURATION_TYPE,
-        ROUND((C.DAILY_FEE - (C.DAILY_FEE * (CRCDP.DISCOUNT_RATE / 100))) * 30) AS FEE
-    FROM 
-        CTE C
-        JOIN CAR_RENTAL_COMPANY_DISCOUNT_PLAN CRCDP ON C.CAR_TYPE = CRCDP.CAR_TYPE
-                                                    AND CRCDP.DURATION_TYPE = "30일 이상"
+SELECT C1.CAR_ID, 
+       C1.CAR_TYPE, 
+       C1.DAILY_FEE * (1 - C2.DISCOUNT_RATE / 100) * 30 AS FEE
+FROM CAR_RENTAL_COMPANY_CAR C1
+    JOIN CAR_RENTAL_COMPANY_DISCOUNT_PLAN C2 ON C1.CAR_TYPE = C2.CAR_TYPE
+                                             AND C2.DURATION_TYPE = "30일 이상"
+WHERE C1.CAR_ID NOT IN (SELECT * FROM CTE)
 )
-SELECT 
-    CAR_ID,
-    CAR_TYPE,
-    FEE
-FROM
-    CTE2
-WHERE
-    FEE >= 500000 AND FEE < 2000000
-ORDER BY 
-    FEE DESC, 
-    CAR_TYPE ASC, 
-    CAR_ID DESC
+SELECT CAR_ID,
+       CAR_TYPE,
+       ROUND(FEE) AS FEE
+FROM CTE2
+WHERE FEE >= 500000 AND FEE < 2000000
+      AND CAR_TYPE IN ('세단', 'SUV')
+ORDER BY 3 DESC, 2, 1 DESC
