c7dc5f0c1a0e7da988158f69cd4647571f81eb78
Ryan <75371366+jyun19980812@users.noreply.github.com>
2025-04-18 14:28:51 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 50.89 ms, Memory: 33 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,10 +1,10 @@
 def solution(participant, completion):
     answer = ''
     d = {}
-    for name in participant:
-        d[name] = d.get(name, 0) + 1
-    for n in completion:
-        d[n] -= 1
+    for i in participant:
+        d[i] = d.get(i, 0) + 1
+    for c in completion:
+        d[c] -= 1
     for k, v in d.items():
         if v > 0:
             answer = k
